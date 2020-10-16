1248b58401ec63c436eac35d011c2e91fe0d0ead
thomas manzi <tmsmanzi@gmail.com>
2020-10-16 15:04:55 -0300

    grafo representado

--- algoritmo_Dijkstra.py
+++ algoritmo_Dijkstra.py
@@ -1,11 +1,13 @@
 def matriz():
-    a=int(input("numero de quantos vertices \n"))
+    a=int(input("Quantos vertices \n"))
+    print(end="\n")
     arr = []
     for i in range(1,a+1):
         linha = []
         for j in range(1,a+1):
-            print("Vertice",i, "Ligando com o vertice",j,"\n")
+            print("Vertice",i, "Ligando com o Vertice",j,"\n")
             b = int(input("Peso da Aresta(caso nao tenha coloque 0)\n"))
+            print(end="\n")
             linha.append(b)
         arr.append(linha)
     
@@ -23,22 +25,23 @@ def matriz():
 
 
 def main():
-    vertice = []
-    vertice = matriz()
-    #print(vertice)
+    grafo = []
+    grafo = matriz()
+    #print(grafo)
     #for para a matriz sair identada
-    for i in range(len(vertice)):
-        print(vertice[i], end="\n")
+    for i in range(len(grafo)):
+        print("Vertice", i+1, end="\n")
+        print(grafo[i], end="\n")
 
-    inicial = int(input("Escolha o vertice inicial"))
-    contInicial = inicial
-    #print(vertice[inicial-1:inicial])    #pegando os valores do vertice incial dentro da matriz representada        
-    inicial = vertice[inicial-1:inicial]
+    inicial = int(input("Escolha o vertice inicial \n"))
+    vertInicial = inicial
+    #print(grafo[inicial-1:inicial])    #pegando os valores do vertice incial dentro da matriz representada        
+    inicial = grafo[inicial-1:inicial]
     #print(inicial)
 
-    #for i in range(len(vertice)):
+    #for i in range(len(grafo)):
         #print(i)
-        #for j in range(len(vertice)):
+        #for j in range(len(grafo)):
 
 
 main()    
